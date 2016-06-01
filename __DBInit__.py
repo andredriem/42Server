441a72ad659d2df1c8673759a8e21dac5ab4b7cd
andredriem <andredriem@gmail.com>
2016-05-31 23:34:20 -0300

    __DBInit__ added

--- __DBInit__.py
+++ __DBInit__.py
@@ -1,6 +1,30 @@
 import sqlite3
-conn = sqlite3.connect('example.db')
+conn = sqlite3.connect('exemple.db')
 
 c = conn.cursor()
 
-c.execute("CREATE TABLE Menu(name CHAR(150),price FLOAT,tag CHAR(250),description CHAR(600),soyUse INT,lactoUse INT,glutUse INT)")
+
+
+##INICIA ESTRUTUR DE DAOS MENU
+
+
+c.execute("""CREATE TABLE menu(name varchar unique primary key,
+				description varchar,
+				glutem integer,
+				vegan integer,
+				vegetarian integer,
+				lactose integer,
+				tipo string,
+				float desconto)""")
+
+#exemplos
+c.execute("""INSERT INTO menu VALUES ('pizza','uma pizza hmmm...',1,1,1,1,'principal',50)""")
+c.execute("""INSERT INTO menu VALUES ('hamburger','uma pizza hmmm...',1,0,0,1,'principal',50)""")
+
+
+
+
+
+
+#c.execute ("""SELECT * FROM menu""")
+#print c.fetchone()
