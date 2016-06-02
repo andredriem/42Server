6087a5834e35adadf8b361747ce7273f0863e0ba
andredriem <andredriem@gmail.com>
2016-06-02 20:29:17 -0300

    Melhorei o BDInit

--- __DBInit__.py
+++ __DBInit__.py
@@ -5,22 +5,48 @@ c = conn.cursor()
 
 
 
-##INICIA ESTRUTUR DE DAOS MENU
+##INICIA ESTRUTUR DE DADOS MENU: Contem dados de todos os pratos
 
 
 c.execute("""CREATE TABLE menu(name varchar unique primary key,
-				description varchar,
-				glutem integer,
-				vegan integer,
-				vegetarian integer,
-				lactose integer,
-				tipo string,
-				float desconto)""")
+			       description varchar,
+			       glutem integer,
+			       vegan integer,
+			       vegetarian integer,
+			       lactose integer,
+			       type string,
+			       discount float)
+                               """)
 
-#exemplos
+#exemplos / dados iniciais
 c.execute("""INSERT INTO menu VALUES ('pizza','uma pizza hmmm...',1,1,1,1,'principal',50)""")
 c.execute("""INSERT INTO menu VALUES ('hamburger','uma pizza hmmm...',1,0,0,1,'principal',50)""")
 
+##INICIA ESTRUTURA DE DADOS PEDIDO: Contem ID de Pedido e Status
+
+c.execute("""CREATE TABLE restaurantOrder(id integer primary key,
+                                table_number integer,
+                                status varchar
+                                )""")
+
+
+#exemplos / dados iniciais
+c.execute("""INSERT INTO restaurantOrder VALUES (0,1,'solicitado')""")
+
+
+##INICIA ESTRUTURA DE DADOS PRATOS-PEDIDOS: RELACIONA PRATO COM PEDIDO
+
+c.execute("""CREATE TABLE orderedDishes(
+          quantity integer,
+          halfportion integer,
+          order_id integer REFERENCES restaurantOrder(id),
+          dish_name varchar REFERENCES menu(name)
+          )""")
+
+
+#exemplos / dados iniciais
+c.execute("""INSERT INTO orderedDishes VALUES (2,0,0,'pizza')""")
+c.execute("""INSERT INTO orderedDishes VALUES (2,1,0,'hamburger')""")
 
 
 
