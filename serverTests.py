cf2593d70eeadfe3a2fa831ff4b7248a70d9eebd
gmmoita <gmmoita@terra.com.br>
2016-06-08 12:36:46 -0300

    Added some ServerTests to menuManager and orderManager.

---
+++ serverTests.py
@@ -0,0 +1,77 @@
+#Server Tests
+
+import sqlite3
+import orderManager
+import menuManager
+import SQLExceptions
+
+conn = sqlite3.connect('exemple.db')
+c = conn.cursor()
+
+#TEST1 - Insert a new Dish
+print menuManager.getMenu()
+menuManager.insertDish('NewDish', 'blablabla', 0, 0, 0, 0, 'principal', 0.0,20.30)
+print menuManager.getMenu()
+
+#TEST2 - Delete a existing Dish
+print "\n"
+print menuManager.getMenu()
+menuManager.deleteDish('NewDish')
+print menuManager.getMenu()
+
+#TEST3 - Delete a not existing Dish
+print "\n"
+print menuManager.getMenu()
+try:
+	menuManager.deleteDish('OAKSDOAKFAOKOA')
+except SQLExceptions.SQLTableDeletionError:
+	pass
+else: 
+	raise
+
+#TEST4 - Insert an existing Dish
+print "\n"
+print menuManager.getMenu()
+menuManager.insertDish('NewDish', 'blablabla', 0, 0, 0, 0, 'principal', 0.0,20.30)
+print menuManager.getMenu()
+try:
+	menuManager.insertDish('NewDish', 'blablabla', 0, 0, 0, 0, 'principal', 0.0,20.30)
+except: 
+	pass
+else:
+	raise
+
+#TEST5 - Insert a new Order
+test = insertOrder(2,'ble')
+addDishToOrder(test,'hamburger',8000,False)
+addDishToOrder(test,'pizza',8001,True)
+print test
+
+#TEST6 - Change an Order Status of an existing Order
+changeOrderStatus(2,'bla')
+
+#TEST7 - Change an Order Status of a not existing Order:
+try:
+	changeOrderStatus(2,'bla')
+except SQLExceptions.SQLTableAlterationError:
+	pass
+else:
+	raise
+
+#TEST8 - Try to add an already existing Dish in an Order
+try:
+	addDishToOrder(test,'hamburger',8000,False)
+except SQLExceptions.SQLTableAlterationError:
+	pass
+else:
+	raise
+
+#TEST9 - Add Dish to a not existing order
+deleteAllDishesFromOrder(test)
+try:
+	addDishToOrder(test,'pizza',8001,True)
+except SQLExceptions.SQLTableInserionError:
+	pass
+else: raise
+
+
